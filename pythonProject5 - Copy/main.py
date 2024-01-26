064b6a2b7421a15ec89971caf8b5bd026f586224
alesolanoc <alejandro.solano.38@gmail.com>
2024-01-26 13:54:44 -0400

    26

--- pythonProject5 - Copy/main.py
+++ pythonProject5 - Copy/main.py
@@ -2,9 +2,5 @@ x = 1
 if x == 1:
     # indented four spaces
     print("x is 1.")
-    
-        print("asdf1")
-            print("asdf1")
-                print("asdf1")
-                    print("asdf1")
+
                     
